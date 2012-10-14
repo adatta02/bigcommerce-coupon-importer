628aff0fb197a03249a9df5c7f8516386293525a
ashish <ashish@ashish-Latitude-D830.(none)>
2012-10-14 17:55:18 -0400

    - added the ability to generate random coupon codes in the injected form. - updated the "matches" field on the manifest to https://*/* so it works with store specific admin paths

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ $(document).ready( function( $ ){
 				
 		$(".ContentContainer")
 			.append( "<form id='sfBigCommForm'>" + addCouponDiv + "</form>"
-					+ "<textarea style='width:500px; height: 300px' id='sfBigcCommCodes' placeholder='Enter codes here...'></textarea>" 
+					+ "Generate codes: <input type='text' placeholder='how many?' id='sfGenerateNum' /><input type='button' id='sfGenerateCodes' value='Generate Codes' /><br>"
+					+ "Already have codes? Enter them below (1 per line)<br><textarea style='width:500px; height: 300px' id='sfBigcCommCodes' placeholder='Enter codes here...'></textarea>" 
 					+ "<input type='button' id='sfBigCommStart' value='Start Import' />");
 		
 		$(".ContentContainer").find(".Panel:last").remove();
@@ -29,13 +30,42 @@ $(document).ready( function( $ ){
 	});
   
     $("#sfBigCommStart").live( "click", function(){
-		
-		var form = $("#sfBigCommForm").serializeArray();
-		
+		var form = $("#sfBigCommForm").serializeArray();	
 		chrome.extension.sendRequest({ msg: "setCodes", codes: $("#sfBigcCommCodes").val(), form: form });
 		window.location = "/admin/index.php?ToDo=createCoupon";
        return false;
     });
+  
+	$("#sfGenerateCodes").live("click", function(){
+		
+		var howMany = $("#sfGenerateNum").val();
+		if( !howMany.length ){
+			alert("Sorry! You need to enter a number to generate.");
+		}		
+		
+		var codes = [];
+		var code;
+		
+		for(var i=0; i < howMany; i++){
+			
+			code = "";
+			for(var j=0; j < 15; j++){
+				
+				if( Math.random() > .3 ){
+					code += String.fromCharCode( Math.floor( ( Math.random() * 26 ) + 65) );
+				}else{
+					code += Math.floor( (Math.random() * 9) + 1 );
+				}
+				
+			}
+			
+			codes.push( code );
+		}
+		
+		$("#sfBigcCommCodes").val( codes.join("\n") );
+		
+		return false;
+	});
   
     $.get("/admin/index.php?ToDo=createCoupon", function(data){
 		addCouponDiv = $(data).find("#div0").html();
